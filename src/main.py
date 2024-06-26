4f347939a687a7b70ad5297c2e49a0665c00930d
nurs <nursultanzhantuar@gmail.com>
2024-06-26 22:18:26 +0500

    Some changes

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import discord
 from discord.ext import commands
 from pixivpy3 import AppPixivAPI    
+import requests
 
 from config import token, refresh_token 
 
@@ -38,15 +39,16 @@ async def joined(ctx, member: discord.Member):
 
 @bot.command(description="Get an illust from pixiv")
 async def illustration(ctx, illust_id: int, image_size: str):
-    image_size = image_size.lower()
-    # Fetch illustration by ID from Pixiv
-    illustration = api.illust_detail(illust_id).illust
-    image_url = illustration["image_urls"][image_size]
-    print(image_url)
+    image_size = image_size.lower() 
 
-    # Create an embed for Discord
-    embed = discord.Embed(title=illustration['title'], url=f"https://www.pixiv.net/en/artworks/{illustration['id']}")
-    embed.set_image(url = image_url)
+    illustration = api.illust_detail(illust_id).illust
+    illustration_url =f"https://www.pixiv.net/en/artworks/{illustration['id']}"
+    image_url = illustration["image_urls"][image_size]
+
+    # Create an embed for Discord 
+
+    embed = discord.Embed(title=illustration['title'], url=illustration_url)
+    embed.set_image(url=image_url)
     embed.set_author(name=f'{illustration["user"]["name"]}')
 
 
